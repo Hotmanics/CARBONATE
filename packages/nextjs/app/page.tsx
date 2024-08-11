c019c1ba091eac3db76fbc6d3b7d9d3f825bd2e1
Jacob Homanics <homanicsjake@gmail.com>
2024-08-10 19:49:13 -0500

    latest

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -6,26 +6,24 @@ const Home: NextPage = () => {
   return (
     <>
       <div className="flex flex-col items-center flex-grow pt-10 space-y-4">
-        <div className="flex flex-col px-5 text-center lg:w-[720px] w-full bg-base-100 rounded-lg shadow-md shadow-secondary">
+        <div className="flex flex-col bg-base-100 px-5 text-center lg:w-[720px] rounded-lg shadow-md shadow-secondary">
           <p className="text-4xl lg:text-7xl beerGlass">Wild Water</p>
           <p className="text-4xl lg:text-7xl beerGlass">Bottle Cap Token</p>
           <p className="text-xl lg:text-2xl beerGlass">A Tokenized Real World Asset</p>
         </div>
-        <div className="flex items-center justify-center px-5 text-center w-full">
-          <div className="w-[680px] flex flex-col items-center">
-            <p className="text-xl lg:text-3xl kanit-light">Civilization has ended?</p>
-            <p className="lg:text-2xl kanit">and</p>
-            <p className="text-xl lg:text-3xl kanit-lght">Your flimsy paper money is now worthless?</p>
-            <p className="text-3xl lg:text-4xl kanit font-bold">
-              Stock up on Wild Water Bottle Cap tokens to be brought into the future!
-            </p>
-          </div>
-        </div>
 
-        <div className="flex flex-col items-center bg-base-100 w-full">
-          <p className="text-6xl beerGlass text-center">FAQ?</p>
+        <div className="flex flex-col items-center text-center w-full px-5">
+          <p className="text-xl lg:text-3xl kanit-light">Civilization has ended?</p>
+          <p className="lg:text-2xl kanit">and</p>
+          <p className="text-xl lg:text-3xl kanit-lght">Your flimsy paper money is now worthless?</p>
+          <p className="text-3xl lg:text-4xl kanit font-bold lg:w-[680px]">
+            Stock up on Wild Water Bottle Cap tokens to be brought into the future!
+          </p>
+        </div>
+        <div className="flex flex-col items-center w-full bg-base-100 px-5">
+          <p className="text-5xl beerGlass text-center">FAQ?</p>
           <div className="lg:w-[620px]">
-            <p className="text-4xl beerGlass text-center">What is it?</p>
+            <p className="text-3xl beerGlass text-center">What is it?</p>
             <p className="text-xl kanit text-center">
               {`A tokenized Real World Asset (RWA) where the supply of the token matches the actual number of bottle caps
             that Jacob Homanics owns in the physical world.`}
@@ -33,7 +31,7 @@ const Home: NextPage = () => {
           </div>
 
           <div className="lg:w-[620px]">
-            <p className="text-4xl beerGlass text-center">How is it?</p>
+            <p className="text-3xl beerGlass text-center">How is it?</p>
             <p className="text-xl kanit text-center">
               {`Jacob loves his mineral water. Every Sunday he takes a a stroll down to the local supermarket to stock up on a 12ct pack of bottles. 
               After poppin' the top off, throughtout the week, he stores the bottle caps for safekeeping instead of throwing them in the trash. 
@@ -42,7 +40,7 @@ const Home: NextPage = () => {
           </div>
 
           <div className="lg:w-[620px]">
-            <p className="text-4xl beerGlass text-center">Why is it?</p>
+            <p className="text-3xl beerGlass text-center">Why is it?</p>
             <p className="text-xl kanit text-center">
               {`This is a fun experiment that will help explore and discover what it means to be a Tokenized Real World
             Asset. It is designed to test and break the boundaries and processes for bringing Real World Assets onchain.`}
