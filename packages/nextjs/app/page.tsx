571bbb6908c1dc28902fe1fbb6b62a170f089bee
Jacob Homanics <homanicsjake@gmail.com>
2024-09-04 00:28:20 -0500

    yay charts

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -54,25 +54,25 @@ function BarChartExample({ chartData }: any) {
   );
 }
 
-// function BarChartExample2({ chartData }: any) {
-//   return (
-//     <ChartContainer config={chartConfig} className="h-[200px] w-auto">
-//       <BarChart accessibilityLayer data={chartData}>
-//         <CartesianGrid vertical={false} />
-//         <XAxis
-//           dataKey="month"
-//           tickLine={false}
-//           tickMargin={10}
-//           axisLine={false}
-//           tickFormatter={value => value.slice(0, 3)}
-//         />
-//         <ChartTooltip content={<ChartTooltipContent />} />
-//         <ChartLegend content={<ChartLegendContent />} />
-//         <Bar dataKey="Transactions: " fill="var(--color-mobile)" radius={4} />
-//       </BarChart>
-//     </ChartContainer>
-//   );
-// }
+function BarChartExample2({ chartData }: any) {
+  return (
+    <ChartContainer config={chartConfig} className="h-[200px] w-auto">
+      <BarChart accessibilityLayer data={chartData}>
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="month"
+          tickLine={false}
+          tickMargin={10}
+          axisLine={false}
+          tickFormatter={value => value.slice(0, 3)}
+        />
+        <ChartTooltip content={<ChartTooltipContent />} />
+        <ChartLegend content={<ChartLegendContent />} />
+        <Bar dataKey="Transactions: " fill="var(--color-mobile)" radius={4} />
+      </BarChart>
+    </ChartContainer>
+  );
+}
 
 const months = [
   "January",
@@ -185,6 +185,75 @@ const Home: NextPage = () => {
 
     setSelectedChartData([...newChartData]);
   }
+
+  useEffect(() => {
+    async function get() {
+      const newChartData = [];
+
+      const observedDate = new Date();
+      observedDate.setUTCDate(observedDate.getUTCDate());
+
+      for (let i = 0; i < 3; i++) {
+        const observedMonth = observedDate.getMonth();
+
+        let newTotal = 0;
+        for (let j = 0; j < mintLogs.length; j++) {
+          const block = await getBlock(wagmiConfig, { blockHash: mintLogs[j].blockHash });
+          const timestampConverted = block.timestamp * BigInt(1000);
+
+          const date = new Date(Number(timestampConverted));
+          const dateYear = date.getUTCFullYear();
+          const dateMonth = date.getUTCMonth();
+
+          const result = dateYear === observedDate.getUTCFullYear() && dateMonth === observedMonth;
+          if (result) {
+            newTotal++;
+          }
+        }
+
+        newChartData.unshift({ month: months[observedMonth], "Transactions: ": newTotal });
+        observedDate.setUTCDate(observedDate.getUTCDate() - 30);
+      }
+
+      setSelectedChartData2([...newChartData]);
+    }
+    get();
+  }, [mintLogs]);
+
+  const [selectedChartData2, setSelectedChartData2] = useState<any>();
+
+  async function setChartData2(numOfMonths: number) {
+    const newChartData = [];
+
+    const observedDate = new Date();
+    observedDate.setUTCDate(observedDate.getUTCDate());
+
+    for (let i = 0; i < numOfMonths; i++) {
+      const observedMonth = observedDate.getMonth();
+      console.log(observedMonth);
+
+      let newTotal = 0;
+      for (let j = 0; j < mintLogs.length; j++) {
+        const block = await getBlock(wagmiConfig, { blockHash: mintLogs[j].blockHash });
+        const timestampConverted = block.timestamp * BigInt(1000);
+
+        const date = new Date(Number(timestampConverted));
+        const dateYear = date.getUTCFullYear();
+        const dateMonth = date.getUTCMonth();
+
+        const result = dateYear === observedDate.getUTCFullYear() && dateMonth === observedMonth;
+        if (result) {
+          newTotal++;
+        }
+      }
+
+      newChartData.unshift({ month: months[observedMonth], "Transactions: ": newTotal });
+      observedDate.setUTCDate(observedDate.getUTCDate() - 30);
+    }
+
+    setSelectedChartData2([...newChartData]);
+  }
+
   return (
     <>
       <div className="flex flex-col flex-grow pt-10 space-y-8 bg-gradient-to-t from-base-100 to-base-200 items-center">
@@ -212,37 +281,73 @@ const Home: NextPage = () => {
           </div>
         </div>
 
-        <div className="w-[200px] flex flex-col w-full items-center justify-center">
-          <p className="text-xl m-4">Token Mint Transactions Per Month</p>
+        <div className="flex flex-wrap">
+          <div className="w-[600px] flex flex-col items-center justify-center">
+            <p className="text-xl m-4">Mint Count</p>
 
-          <div className="flex flex-wrap space-x-2">
-            <button
-              className="btn btn-sm btn-secondary"
-              onClick={() => {
-                setChartData(3);
-              }}
-            >
-              3 month
-            </button>
-            <button
-              className="btn btn-sm btn-secondary"
-              onClick={() => {
-                setChartData(6);
-              }}
-            >
-              6 month
-            </button>
-            <button
-              className="btn btn-sm btn-secondary"
-              onClick={() => {
-                setChartData(12);
-              }}
-            >
-              YTD
-            </button>
+            <div className="flex flex-wrap space-x-2">
+              <button
+                className="btn btn-sm btn-secondary"
+                onClick={() => {
+                  setChartData(3);
+                }}
+              >
+                3 month
+              </button>
+              <button
+                className="btn btn-sm btn-secondary"
+                onClick={() => {
+                  setChartData(6);
+                }}
+              >
+                6 month
+              </button>
+              <button
+                className="btn btn-sm btn-secondary"
+                onClick={() => {
+                  setChartData(12);
+                }}
+              >
+                YTD
+              </button>
+            </div>
+            <div className="m-4">
+              <BarChartExample chartData={selectedChartData} />
+            </div>
           </div>
-          <div className="m-4">
-            <BarChartExample chartData={selectedChartData} />
+
+          <div className="w-[600px] flex flex-col items-center justify-center">
+            <p className="text-xl m-4">Transactions</p>
+
+            <div className="flex flex-wrap space-x-2">
+              <button
+                className="btn btn-sm btn-secondary"
+                onClick={() => {
+                  setChartData2(3);
+                }}
+              >
+                3 month
+              </button>
+              <button
+                className="btn btn-sm btn-secondary"
+                onClick={() => {
+                  setChartData2(6);
+                }}
+              >
+                6 month
+              </button>
+              <button
+                className="btn btn-sm btn-secondary"
+                onClick={() => {
+                  setChartData2(12);
+                }}
+              >
+                YTD
+              </button>
+            </div>
+            <div className="m-4">
+              <BarChartExample2 chartData={selectedChartData2} />
+            </div>
           </div>
         </div>
 
